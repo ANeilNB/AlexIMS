abf7daa9bbcdacd23615e7c103aa7149f56efc79
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-07-13 13:32:15 +0100

    Changed look and feel to system instead of Nimbus due to Docker compatibility.

--- src/alexIMS/IMSRunner.java
+++ src/alexIMS/IMSRunner.java
@@ -25,15 +25,11 @@ public class IMSRunner {
 	public static void main(String[] args) {
 		
 
-		try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
-		            break;
-		        }
-		    }
-		} catch (Exception e) {
-		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		try{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch(Exception e){
+			//UIManager error of some kind.
 		}
 		
 		IMSModel model = new IMSModel();
